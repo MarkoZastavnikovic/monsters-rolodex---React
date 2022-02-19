ebcf971611209eb682cd8830fffd904a3a69e046
MarkoZastavnikovic <marko.zastavnikovic@hotmail.com>
2022-02-19 03:22:30 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    (async function () {
+      try {
+        // console.log("cDM start");
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        const data = await res.json();
+        // console.log("before setState");
+        this.setState({ monsters: data });
+        // console.log("cDM end");
+      } catch (err) {
+        console.log(err);
+      }
+    }.call(this));
+  }
+
+  render() {
+    // console.log("on render start");
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
